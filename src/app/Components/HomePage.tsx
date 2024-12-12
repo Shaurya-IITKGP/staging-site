b0ab07482e58c90d7c9bdf4f6a1bbd4719e47e39
pranjulCoral <pranjul@coralacademy.com>
2024-12-12 20:04:09 +0530

    Pranjul:Image added

--- src/app/Components/HomePage.tsx
+++ src/app/Components/HomePage.tsx
@@ -1,8 +1,8 @@
-// ShauryaMainSite/src/app/Components/HomePage.tsx
 "use client";
 import { useEffect, useState } from "react";
 import Footer from "./footer";
 import Animate from "./animate";
+import Image from "next/image";
 
 export default function MainPage() {
   const [scrollY, setScrollY] = useState(0);
@@ -35,31 +35,36 @@ export default function MainPage() {
 
   return (
     <>
-    <div className="min-h-screen relative bg-cover bg-center flex flex-col overflow-hidden">
-      {/* Background Image */}
-      <div className="absolute inset-0 bg-gradient-to-r from-gray-900 to-black opacity-80 h-full"></div>
-      <div className="fixed inset-0 bg-[url('/mainPage2.jpg')] bg-cover opacity-30"></div>
-
-      {/* Shaurya Logo with Glowing Effect and Flames */}
-      <div className="z-20 flex flex-col md:justify-center md:items-center md:mt-0 mt-60  w-full">
-        <div className="relative">
-          <img
-            src="/shaurya_dark_brown-removebg-preview.png"
-            alt="Shaurya Logo"
-            className="w-[700px] h-auto object-contain items-center logo-glow"
+      <div className="min-h-screen relative bg-cover bg-center flex flex-col overflow-hidden">
+        {/* Background Image */}
+        <div className="absolute inset-0 bg-gradient-to-r from-gray-900 to-black opacity-80 h-full"></div>
+        <div className="fixed inset-0 z-0">
+          <Image
+            src="/mainPage2.jpg"
+            alt="Background"
+            layout="fill"
+            objectFit="cover"
+            className="opacity-30"
           />
         </div>
-        <div className="relative">
-        <Animate/>
+
+        {/* Shaurya Logo with Glowing Effect and Flames */}
+        <div className="z-20 flex flex-col md:justify-center md:items-center md:mt-0 mt-60 w-full">
+          <div className="relative">
+            <Image
+              src="/shaurya_dark_brown-removebg-preview.png"
+              alt="Shaurya Logo"
+              width={700}
+              height={200}
+              className="w-[700px] h-auto object-contain items-center logo-glow"
+            />
+          </div>
+          <div className="relative">
+            <Animate />
+          </div>
         </div>
-        
       </div>
-
-     
-
-
-    </div>
-      <Footer/>
-</>
+      <Footer />
+    </>
   );
 }
