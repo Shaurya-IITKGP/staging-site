6a9a4a7a7425cc56102c4bbaa0bb4b697cb5381b
Sahil-Sinha-11 <sahilsinha6969@gmail.com>
2024-12-12 13:11:38 +0530

    Image

--- src/app/Components/HomePage.tsx
+++ src/app/Components/HomePage.tsx
@@ -39,8 +39,9 @@ export default function MainPage() {
     <>
     <div className="min-h-screen relative bg-cover bg-center flex flex-col overflow-hidden">
       {/* Background Image */}
-      <div className="absolute inset-0 bg-gradient-to-r from-gray-900 to-black opacity-80 h-full"></div>
-      <div className="fixed inset-0 bg-[url('/mainPage2.jpg')] bg-cover opacity-30"></div>
+      <div className="absolute inset-0 bg-gradient-to-r from-gray-900 to-black opacity-80 h-full"
+      style={{ backgroundImage: `url(/mainPage2.jpg)` }}></div>
+      <div className="fixed inset-0  bg-cover opacity-30"></div>
 
       {/* Shaurya Logo with Glowing Effect and Flames */}
       <div className="z-20 flex flex-col md:justify-center md:items-center md:mt-0 mt-60  w-full">
