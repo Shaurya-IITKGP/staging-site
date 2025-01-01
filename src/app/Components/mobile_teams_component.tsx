e9cec0f1397b8e59c395226256d8dceae9a0ba13
DhruvGaur32 <154977139+DhruvGaur32@users.noreply.github.com>
2025-01-01 23:52:37 +0530

    Teams Page fixed

--- src/app/Components/mobile_teams_component.tsx
+++ src/app/Components/mobile_teams_component.tsx
@@ -1,17 +1,19 @@
 import CardMobile from './mobile_teams_card';
 import React from 'react'
 
+interface Props{
+  team:string[][]
+}
 
-
-const CardsmobileComponent = () => {
-    const name_portfolio: [string, string][] = [['Tejashwi Kumar Jha', 'Executive'],['Jival Chorawala', 'Executive'], ['Chavi Agarwal', 'Executive']];
+const CardsmobileComponent:React.FC<Props> = ({team}) => {
+    // const name_portfolio: [string, string][] = [['Tejashwi Kumar Jha', 'Executive'],['Jival Chorawala', 'Executive'], ['Chavi Agarwal', 'Executive']];
 
   return (
     <div className='flexjustify-center flex-row mt-16 z-10'>
      <div>
         <ul className='grid'>
-          {name_portfolio.map((item, index) => (
-            <li key={index}><div className={`k3:gap-10 gap-0 grid`}><CardMobile name={name_portfolio[index][0]} portfolio={name_portfolio[index][1]}/><div/></div></li>
+          {team.map((item, index) => (
+            <li key={index}><div className={`k3:gap-10 gap-0 grid`}><CardMobile name={team[index][0]} portfolio={team[index][1]} i={team[index][2]} l={team[index][4]} f={team[index][3]} m={team[index][5]}/><div/></div></li>
           ))}
         </ul>
       </div> 
